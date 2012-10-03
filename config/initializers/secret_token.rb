41e7c06f659e6ad34e47cbab04cc213fd12b9f0a
Mark Serrano <mserran2@swarthmore.edu>
2012-10-03 02:30:16 -0400

    - Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AirportShuttle::Application.config.secret_token = 'f4ef257a1bf45249e6c4282192ace238b4f32ef6713ee0079046b4f70f72e8ba7eaceeb031976bdcf7664dd9d166123b69a328349207d9fd3c31412e9d0c6a99'
